d31ce24e874e43b020cabe1c1079d2207cb8ce76
Gavin Carew <gjcarew@gmail.com>
2023-02-15 23:45:28 -0700

    Create PromptBuilder and unit tests for PromptBuilder

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -4,12 +4,9 @@ class JobsController < ApplicationController
   def create
     require 'pry';binding.pry
     job = Job.new(job_params)
-    
+
     if job.valid?
-      prompt = PromptBuilder.new(job_params, current_user.profile)
-      letter_text = OpenAI::TextService.new.cover_letter(prompt.text)
-      job.letter_text = letter_text
-      job.save
+      add_letter_to(job)
       redirect_to job_path(job)
     else
       flash.now[:messages] = job.errors.full_messages[0]
@@ -25,5 +22,12 @@ class JobsController < ApplicationController
 
   def job_params
     params.permit(:role, :company, :description)
+  end
+
+  def add_letter_to(job)
+    prompt = PromptBuilder.new(job, current_user.profile)
+    letter_text = OpenAI::TextService.new.cover_letter(prompt.text)
+    job.letter_text = letter_text
+    job.save
   end
 end
---
+++ spec/poros/prompt_builder_spec.rb
@@ -0,0 +1,29 @@
+require 'rails_helper'
+
+RSpec.describe :PromptBuilder do 
+  it 'Generates a prompt from a job and a profile' do 
+    user = User.create!(email: 'example@user.com', password: 'ValidPW')
+    Profile.create!(user_id: user.id, 
+                skills: 'Ruby',
+                experience: 'I worked at Amazon as a software engineer',
+                education: 'Turing School of Software and Design',
+                projects: 'Cover Getter, a rails project using ChatGPT')
+    job = Job.new(role: 'Software engineer',
+                  company: 'Google',
+                  description: 'Make a search engine')
+    prompt = PromptBuilder.new(job, user.profile)
+
+    expect(prompt).to be_a PromptBuilder
+    expect(prompt.text).to eq("Write me a cover letter. \nThe company is Google. \nThe job title is Software engineer. \nThis is the job description: Make a search engine \nThese are my skills: Ruby \nThis is my relevant experience: I worked at Amazon as a software engineer \nThis is my education: Turing School of Software and Design \nThese are projects I've worked on: Cover Getter, a rails project using ChatGPT")
+  end
+  
+  it 'Can generate a prompt with an incomplete job and profile' do
+    user = User.create!(email: 'example@user.com', password: 'ValidPW')
+    Profile.create!(user_id: user.id)
+    job = Job.new(role: 'Software engineer',
+      company: 'Google')
+      prompt = PromptBuilder.new(job, user.profile)
+
+      expect(prompt.text).to eq("Write me a cover letter. \nThe company is Google. \nThe job title is Software engineer. \n")
+  end
+end
